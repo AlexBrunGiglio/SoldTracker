1e153994a88aaf951f60e35781fe8733fe023b50
Alexandre BG <alexandre.brungiglio@nextalys.com>
2021-11-25 11:21:48 +0100

    Restart project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'soldtracker',
+    appId: '1:569304007788:web:da27e4f891560d41fd8449',
+    storageBucket: 'soldtracker.appspot.com',
+    apiKey: 'AIzaSyCdzwaza-SBu7hxbQeBTE7lHH_ShL3GQbc',
+    authDomain: 'soldtracker.firebaseapp.com',
+    messagingSenderId: '569304007788',
+    measurementId: 'G-XPXM6SW0WK',
+  },
   production: true
 };
