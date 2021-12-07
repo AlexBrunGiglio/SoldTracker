a406014319d9a90eb5cdd653e5e3068cd2533bfd
Alexandre BG <alexandre.brungiglio@nextalys.com>
2021-12-07 12:44:11 +0100

    login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,24 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
-import {
-  collection,
-  doc,
-  docData,
-  DocumentReference,
-  CollectionReference,
-  Firestore,
-  onSnapshot,
-  query,
-  where,
-  Unsubscribe,
-  Query,
-  DocumentData,
-  collectionData,
-  collectionChanges,
-  docSnapshots,
-} from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { UserDto } from '../../../core/database/users/user-dto';
 
 @Component({
   selector: 'app-login',
@@ -27,9 +11,12 @@ import {
   encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent implements OnInit {
-  userData: any;
-  items: any;
+  user: UserDto = new UserDto();
+  loading = false;
   constructor(
+    private afAuth: AngularFireAuth,
+    public alertCtrl: AlertController,
+    private router: Router
   ) {
   }
 
@@ -37,7 +24,24 @@ export class LoginComponent implements OnInit {
 
   }
 
-  // signIn(email, password) {
-  //   return this.ngFireAuth.auth.signInWithEmailAndPassword(email, password)
-  // }
+  async signIn() {
+    this.loading = true;
+    if (!this.user.email || !this.user.password) {
+      this.loading = false;
+      return;
+    }
+    try {
+      const login = await this.afAuth.signInWithEmailAndPassword(this.user.email, this.user.password);
+      this.router.navigateByUrl('/tabs/home');
+    } catch (error) {
+      const alert = await this.alertCtrl.create({
+        animated: true,
+        message: error.message,
+        translucent: true,
+        mode: 'ios'
+      });
+      await alert.present();
+    }
+    this.loading = false;
+  }
 }
