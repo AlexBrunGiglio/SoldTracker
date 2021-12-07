9b30f823c26b3d0a6a9c614f708d0f82f132c5c7
Alexandre BG <alexandre.brungiglio@nextalys.com>
2021-12-07 17:06:10 +0100

    check uid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseAppModule } from '@angular/fire/app';
 import { getAuth, onAuthStateChanged, provideAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
 import { Capacitor } from '@capacitor/core';
 import { StatusBar } from '@capacitor/status-bar';
 import 'firebase/firestore';
@@ -15,12 +16,20 @@ import 'firebase/firestore';
 export class AppComponent implements OnInit {
   constructor(
     private firestore: AngularFirestore,
+    private router: Router,
   ) {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
       if (user) {
         const uid = user.uid;
+        if (!uid) {
+          this.router.navigateByUrl('/login');
+        }
+        else {
+          this.router.navigateByUrl('/tabs/home');
+        }
       } else {
+        this.router.navigateByUrl('/login');
       }
     });
   }
