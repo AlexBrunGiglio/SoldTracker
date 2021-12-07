12efd3890422984e76b3af10322c3898773f4a45
Alexandre BG <alexandre.brungiglio@nextalys.com>
2021-12-07 15:26:34 +0100

    logout

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,4 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { getAuth, signOut } from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -6,8 +10,31 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['settings.page.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class SettingsPage {
+export class SettingsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    public afAuth: AngularFireAuth,
+    public alertCtrl: AlertController,
+  ) { }
 
+  ngOnInit() {
+
+  }
+
+  async logout() {
+    try {
+      const auth = getAuth();
+      const signout = await signOut(auth);
+      this.router.navigateByUrl('/login');
+    } catch (error) {
+      const alert = await this.alertCtrl.create({
+        animated: true,
+        message: error.message,
+        translucent: true,
+        mode: 'ios'
+      });
+      await alert.present();
+    }
+  }
 }
