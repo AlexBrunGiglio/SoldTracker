8eaed65fe5c24b5a421354541f01b8a231e5b4d7
Alexandre BG <alexandre.brungiglio@nextalys.com>
2021-12-08 16:46:21 +0100

    clean app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FirebaseAppModule } from '@angular/fire/app';
-import { getAuth, onAuthStateChanged, provideAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { setDoc, doc, Firestore } from '@angular/fire/firestore';
+import { getAuth, onAuthStateChanged } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { Capacitor } from '@capacitor/core';
 import { StatusBar } from '@capacitor/status-bar';
-import 'firebase/firestore';
-import { UsersService } from '../core/database/users/users.service';
-import { CategoriesType } from '../environments/constant';
-
-
-@Component({
+import 'firebase/firestore'; @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
   constructor(
-    private firestore: AngularFirestore,
     private router: Router,
-    private db: Firestore,
-    private userService: UsersService,
   ) {
     const auth = getAuth();
     onAuthStateChanged(auth, async (user) => {
