2401d47f209e46a78728865717b8f944e2e425fa
Alexandre BG <alexandre.brungiglio@nextalys.com>
2021-12-08 17:01:39 +0100

    global stats page

--- src/app/statistics/stats.page.ts
+++ src/app/statistics/stats.page.ts
@@ -1,4 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { getAuth } from '@angular/fire/auth';
+import { doc, Firestore, onSnapshot } from '@angular/fire/firestore';
+import { UserDto } from '../../core/database/users/user-dto';
 
 @Component({
   selector: 'app-stats',
@@ -6,8 +9,33 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['stats.page.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class StatsPage {
+export class StatsPage implements OnInit {
+  user: UserDto;
+  negativesOperations = 0;
+  positivesOperations = 0;
+  constructor(
+    private db: Firestore,
+  ) { }
 
-  constructor() { }
+  async ngOnInit() {
+    const auth = await getAuth();
+    const unsub = onSnapshot(doc(this.db, 'users', auth.currentUser.uid), (document) => {
+      this.user = document.data() as UserDto;
+      this.getUserStats();
+    });
+  }
 
+  getUserStats() {
+    if (!this.user) {
+      return;
+    }
+    const neg = this.user.transactions.filter(x => x.value < 0);
+    for (const item of neg) {
+      this.negativesOperations += item.value;
+    }
+    const pos = this.user.transactions.filter(x => x.value > 0);
+    for (const item of pos) {
+      this.positivesOperations += item.value;
+    }
+  }
 }
