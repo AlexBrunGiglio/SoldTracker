87fb3096884ce34f7b8ab63dc56fd444879af403
Alexandre BG <alexandre.brungiglio@nextalys.com>
2021-12-10 12:14:42 +0100

    improve

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -5,6 +5,10 @@ import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { BatteryInfo, Device, DeviceInfo } from '@capacitor/device';
 import { routesList } from '../../environments/routes';
+import { UsersService } from '../../core/database/users/users.service';
+import { Firestore } from '@angular/fire/firestore';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { UserDto } from '../../core/database/users/user-dto';
 
 @Component({
   selector: 'app-settings',
@@ -15,15 +19,22 @@ import { routesList } from '../../environments/routes';
 export class SettingsPage implements OnInit {
   batteryInfo: BatteryInfo;
   deviceInfo: DeviceInfo;
+  routesList = routesList;
+  user: UserDto;
   constructor(
     private router: Router,
     public afAuth: AngularFireAuth,
     public alertCtrl: AlertController,
+    private db: Firestore,
   ) { }
 
   async ngOnInit() {
     this.deviceInfo = await Device.getInfo();
     this.batteryInfo = await Device.getBatteryInfo();
+    const auth = await getAuth();
+    const unsub = onSnapshot(doc(this.db, 'users', auth.currentUser.uid), (document) => {
+      this.user = document.data() as UserDto;
+    });
   }
 
   async logout() {
